58caee81625bffc3ea67e313c32704663fa1a4af
Geoff Gerrietts <ggerrietts@compete.com>
2011-11-05 10:57:17 -0400

    skeletal Django model & view for our contact model

--- address/models.py
+++ address/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+import common.models
 
 # Create your models here.
+
+class ContactModel(models.Model, common.models.AjaxModelHelper):
+	email = models.EmailAddressField()
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
--- urls.py
+++ urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from address.views import AsyncContactView
+
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'spine.views.home', name='home'),
-    # url(r'^spine/', include('spine.foo.urls')),
+    url(AsyncContactView.make_url(), AsyncContactView.as_view(), name='async_contact_view'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),

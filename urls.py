5a904f84c3ee593054274f30781bf58d02e6b6c5
Geoff Gerrietts <ggerrietts@compete.com>
2011-11-08 01:16:34 -0500

    working skeletal address app, take 2

--- urls.py
+++ urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
+
+from django.views.generic import TemplateView
 
 from address.views import AsyncContactView
 
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(AsyncContactView.make_url(), AsyncContactView.as_view(), name='async_contact_view'),
+    url(r'contacts', TemplateView.as_view(template_name="address/contact.html"), name="contact_view"),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 

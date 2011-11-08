c6952d2f8fa3feab3ff0620243e56d92e664aa05
Geoff Gerrietts <ggerrietts@compete.com>
2011-11-08 01:18:04 -0500

    added common to apps list for static & template loading

--- settings.py
+++ settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'common',
 )
 
 # A sample logging configuration. The only tangible logging

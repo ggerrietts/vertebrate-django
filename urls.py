f655f3630d250a4a57b20edcee548f738664a0fa
Geoff Gerrietts <ggerrietts@compete.com>
2011-11-08 01:23:05 -0500

    making statics work properly -- forgot the urls

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
@@ -12,3 +13,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()

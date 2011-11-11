f5c014ad026e25910073d8fcaa89db44aa134f4a
Geoff Gerrietts <geoff@gerrietts.net>
2011-11-11 13:03:29 -0500

    change to dict() instead of dictionary comprehension to stay compatible with older pythons

--- common/models.py
+++ common/models.py
@@ -1,4 +1,4 @@
 class AjaxModelHelper(object):
 
 	def toJSON(self):
-		return {fld.name: getattr(self, fld.name) for fld in self._meta.fields}
+		return dict((fld.name, getattr(self, fld.name)) for fld in self._meta.fields)
